724a0fdc9e6481a2cf967498a0b0dff1da240d4b
romansg <romans@unam.mx>
2018-03-07 15:02:51 -0600

    Inicio de la interfaz de la actividad de resumen de datos

--- app/src/main/java/com/romansg/curso3semana2/MainActivity.java
+++ app/src/main/java/com/romansg/curso3semana2/MainActivity.java
@@ -1,13 +1,37 @@
 package com.romansg.curso3semana2;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout tiNombreCompleto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tiNombreCompleto = findViewById(R.id.tiNombreCompleto);
+
+        Button btnSiguiente = findViewById(R.id.btnSiguiente);
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verResumenDatos();
+            }
+        });
+    }
+
+    public void verResumenDatos() {
+        Intent intent = new Intent(this, ResumenDatosActivity.class);
+
+        String nombreCompleto = tiNombreCompleto.getEditText().getText().toString();
+        intent.putExtra("nombreCompleto", nombreCompleto);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/romansg/curso3semana2/ResumenDatosActivity.java
+++ app/src/main/java/com/romansg/curso3semana2/ResumenDatosActivity.java
@@ -2,12 +2,22 @@ package com.romansg.curso3semana2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResumenDatosActivity extends AppCompatActivity {
+    private TextView tvNombreCompleto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumen_datos);
+
+        tvNombreCompleto = findViewById(R.id.tvNombreCompleto);
+
+        Bundle extras = getIntent().getExtras();
+
+        String nombreCompleto = extras.getString("nombreCompleto");
+
+        tvNombreCompleto.setText(nombreCompleto);
     }
 }
