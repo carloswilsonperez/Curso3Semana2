799a433303216b6aa2f80dd3091fc6883489592e
romansg <romans@unam.mx>
2018-03-08 11:37:53 -0600

    Finalizada la programación del comportamiento de la app.

--- app/src/main/java/com/romansg/curso3semana2/MainActivity.java
+++ app/src/main/java/com/romansg/curso3semana2/MainActivity.java
@@ -6,10 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputLayout tiNombreCompleto;
+    private DatePicker dpFechaNacimiento;
     private TextInputLayout tiTelefono;
     private TextInputLayout tiEmail;
     private TextInputLayout tiDescripcion;
@@ -20,11 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tiNombreCompleto = findViewById(R.id.main_tiNombreCompleto);
-        tiTelefono = findViewById(R.id.main_tiTelefono);
-        tiEmail = findViewById(R.id.main_tiEmail);
-        tiDescripcion = findViewById(R.id.main_tiDescripcion);
-        btnSiguiente = findViewById(R.id.main_btnSiguiente);
+        asociarControles();
 
         btnSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,15 +35,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Asocia los controles de la interfaz con los miembros privados
+     */
+    public void asociarControles() {
+        tiNombreCompleto = findViewById(R.id.main_tiNombreCompleto);
+        dpFechaNacimiento = findViewById(R.id.main_dpFechaNacimiento);
+        tiTelefono = findViewById(R.id.main_tiTelefono);
+        tiEmail = findViewById(R.id.main_tiEmail);
+        tiDescripcion = findViewById(R.id.main_tiDescripcion);
+        btnSiguiente = findViewById(R.id.main_btnSiguiente);
+    }
+
     public void verResumenDatos() {
         Intent intent = new Intent(this, ResumenDatosActivity.class);
 
+        // Extraer datos de los controles
         String nombreCompleto = tiNombreCompleto.getEditText().getText().toString();
         String telefono = tiTelefono.getEditText().getText().toString();
         String email = tiEmail.getEditText().getText().toString();
         String descripcion = tiDescripcion.getEditText().getText().toString();
 
+        int dia = dpFechaNacimiento.getDayOfMonth();
+        int mes = dpFechaNacimiento.getMonth();
+        int año = dpFechaNacimiento.getYear();
+
+        // Dar formato a la fecha (tomado de https://stackoverflow.com/questions/14851285/how-to-get-datepicker-value-in-date-format)
+        Calendar calendario = Calendar.getInstance();
+        calendario.set(año, mes, dia);
+        SimpleDateFormat sdf = new SimpleDateFormat("d 'de' MMMM 'de' yyyy");
+        String fechaNacimiento = sdf.format(calendario.getTime());
+
+        // Pasar datos a la siguiente actividad
         intent.putExtra("nombreCompleto", nombreCompleto);
+        intent.putExtra("fechaNacimiento", fechaNacimiento);
         intent.putExtra("telefono", telefono);
         intent.putExtra("email", email);
         intent.putExtra("descripcion", descripcion);
--- app/src/main/java/com/romansg/curso3semana2/ResumenDatosActivity.java
+++ app/src/main/java/com/romansg/curso3semana2/ResumenDatosActivity.java
@@ -1,13 +1,16 @@
 package com.romansg.curso3semana2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class ResumenDatosActivity extends AppCompatActivity {
     private TextView tvNombreCompleto;
+    private TextView tvFechaNacimiento;
     private TextView tvTelefono;
     private TextView tvEmail;
     private TextView tvDescripcion;
@@ -18,11 +21,7 @@ public class ResumenDatosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumen_datos);
 
-        tvNombreCompleto = findViewById(R.id.resumen_datos_tvNombreCompleto);
-        tvTelefono = findViewById(R.id.resumen_datos_tvTelefono);
-        tvEmail = findViewById(R.id.resumen_datos_tvEmail);
-        tvDescripcion = findViewById(R.id.resumen_datos_tvDescripcion);
-        btnEditar = findViewById(R.id.main_btnEditar);
+        asociarControles();
 
         btnEditar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,18 +30,29 @@ public class ResumenDatosActivity extends AppCompatActivity {
             }
         });
 
-
-
+        // Obtener datos pasados
         Bundle extras = getIntent().getExtras();
 
         String nombreCompleto = extras.getString("nombreCompleto");
+        String fechaNacimiento = extras.getString("fechaNacimiento");
         String telefono = extras.getString("telefono");
         String email = extras.getString("email");
         String descripcion = extras.getString("descripcion");
 
+        // Llenar controles
         tvNombreCompleto.setText(nombreCompleto);
+        tvFechaNacimiento.setText(fechaNacimiento);
         tvTelefono.setText(telefono);
         tvEmail.setText(email);
         tvDescripcion.setText(descripcion);
+    }
+
+    public void asociarControles() {
+        tvNombreCompleto = findViewById(R.id.resumen_datos_tvNombreCompleto);
+        tvFechaNacimiento = findViewById(R.id.resumen_datos_tvFechaNacimiento);
+        tvTelefono = findViewById(R.id.resumen_datos_tvTelefono);
+        tvEmail = findViewById(R.id.resumen_datos_tvEmail);
+        tvDescripcion = findViewById(R.id.resumen_datos_tvDescripcion);
+        btnEditar = findViewById(R.id.main_btnEditar);
     }
 }
